13f4cf6060aa293013082f4d56de05e326b6b7fe
George G <kreach3r@users.noreply.github.com>
2015-12-26 14:51:50 +0200

    Layers: dynamic backup script
    Backup all overlay apks located inside /system/vendor/overlay dynamically, during dirty flash.
    No more hardcoding, Tasker plugins, external apps.
    
    A script by @KreAch3R
    
    update by @ezio84: make the script only if we are on hammerhead,
    not for devices like Angler that don't need to backup the layers
    (they are in /vendor/ instead of /system/vendor/)

--- products/nexus_hammerhead.mk
+++ products/nexus_hammerhead.mk
@@ -30,3 +30,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
     BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30M/2862625:user/release-keys \
     PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30M 2862625 release-keys"
+
+# Layers Backup
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/common/hammerhead/addon.d/71-layers.sh:system/addon.d/71-layers.sh
