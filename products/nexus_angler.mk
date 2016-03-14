a2dafa201a95bed661ced4d879b4d08928b47442
ezio84 <brabus84@gmail.com>
2016-03-14 16:07:29 +0100

    Layers script is useless on 6p and 5x due to the vendor img

--- products/nexus_angler.mk
+++ products/nexus_angler.mk
@@ -33,6 +33,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Addtional backup tools for dirty flash
 PRODUCT_COPY_FILES += \
-    vendor/nexus/prebuilt/common/genericdevice/addon.d/71-layers.sh:system/addon.d/71-layers.sh \
     vendor/nexus/prebuilt/common/genericdevice/addon.d/99-dpi.sh:system/addon.d/99-dpi.sh \
     vendor/nexus/prebuilt/common/genericdevice/addon.d/91-font.sh:system/addon.d/91-font.sh
